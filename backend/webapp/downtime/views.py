41ec443981696479f2c050a91d954fce40ac3505
alekgurin <alek.gurin@gmail.com>
2023-09-18 20:30:42 +0100

    cleanup

--- backend/webapp/downtime/views.py
+++ backend/webapp/downtime/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, mixins
+from rest_framework import generics
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 
